a0b1a88715a1e30211ecbe8d51e6a8a0b41de79f
CityTropes <jan.depelsmacker@gmail.com>
2021-12-27 16:22:44 +0100

    programming 2 buttons

---
+++ Arduino/MyTwooButtons/Digital_Inputs_2_buttons/Digital_Inputs_2_buttons.ino
@@ -0,0 +1,27 @@
+// programming 2 connected buttons (Input) and a ledPin (Output)
+
+int ledPin = 5;
+int buttonApin = 9;
+int buttonBpin = 8;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(buttonBpin, INPUT_PULLUP);  
+}
+
+void loop() 
+{
+
+  //When a switch/button is pressed, it connects the input pin to GND, so that it is no longer HIGH
+  
+  if (digitalRead(buttonApin) == LOW)  //if button A is pressed and circuit is broken (input pin to GND)
+  {
+    digitalWrite(ledPin, HIGH);       //turn on connected LED
+  }
+  if (digitalRead(buttonBpin) == LOW) //if button B is pressed and circuit is broken (input pin to GND)
+  {
+    digitalWrite(ledPin, LOW);        //turn off connected LED
+  }
+}
